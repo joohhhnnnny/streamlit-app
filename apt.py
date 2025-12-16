ab03c0863e2e952637ac0661aaa24406e19f30c6
John Benedict Bongcac <johnbongcacjohn@gmail.com>
2025-12-16 22:39:20 +0800

    sge pa bai

--- apt.py
+++ apt.py
@@ -33,7 +33,7 @@ model_choice = st.sidebar.radio(
 )
 
 MODEL_PATHS = {
-    "MobileNetV2": "models/mobilenetv2.keras",
+    "MobileNetV2": "models/mobilenetv2.h5",
     "YOLOv8": "models/best-yolov8s-v2.pt"
 }
 
